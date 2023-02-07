65b78f267ccf375710d1393c788b0f3a58958ad9
zhaoli <lizhao15431230@icloud.com>
2023-02-07 18:30:54 +0800

    100. 相同的树

---
+++ src/binary_tree/100. 相同的树.ts
@@ -0,0 +1,16 @@
+// 100. 相同的树
+// 简单
+// https://leetcode.cn/problems/same-tree/description/?show=1
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+  if(p === null && q === null) {
+      return true
+  }
+  if(p === null || q === null) {
+      return false
+  }
+  if(p.val !== q.val) {
+      return false
+  }
+  
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+};
