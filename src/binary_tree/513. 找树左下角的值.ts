7551facb1ef5c7300caecd249803a0e8f07de318
papazhao <lizhao15431230@hotmail.com>
2023-02-08 22:14:04 +0800

    513. 找树左下角的值

---
+++ src/binary_tree/513. 找树左下角的值.ts
@@ -0,0 +1,20 @@
+// 513. 找树左下角的值
+// 中等
+// https://leetcode.cn/problems/find-bottom-left-tree-value/description/
+function findBottomLeftValue(root: TreeNode | null): number {
+  let queue = [root], ans = 0
+  while (queue.length) {
+      ans = queue[0].val
+      const next: TreeNode[] = []
+      for (const node of queue) {
+          if (node.left) {
+              next.push(node.left)
+          }
+          if (node.right) {
+              next.push(node.right)
+          }
+      }
+      queue = next
+  }
+  return ans
+};
