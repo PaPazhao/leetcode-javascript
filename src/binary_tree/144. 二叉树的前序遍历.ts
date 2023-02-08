b4f3daa4f4f339ced35fc5331829ecc565504465
zhaoli <lizhao15431230@icloud.com>
2023-02-08 15:39:01 +0800

    144. 二叉树的前序遍历

---
+++ src/binary_tree/144. 二叉树的前序遍历.ts
@@ -0,0 +1,11 @@
+// 144. 二叉树的前序遍历
+// 简单
+// https://leetcode.cn/problems/binary-tree-preorder-traversal/
+function preorderTraversal(root: TreeNode | null): number[] {
+  if (root === null) {
+      return []
+  }
+  return [root.val]
+      .concat(preorderTraversal(root.left))
+      .concat(preorderTraversal(root.right))
+};
